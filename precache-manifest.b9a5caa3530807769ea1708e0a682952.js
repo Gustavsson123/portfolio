80788202762c74074378c0d627a8847465f7c67d
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2023-04-18 10:17:35 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b9a5caa3530807769ea1708e0a682952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eb76dc46dd3e0462c0d",
+    "url": "/react_portfolio_ghpages/static/css/main.54d1e694.chunk.css"
+  },
+  {
+    "revision": "4eb76dc46dd3e0462c0d",
+    "url": "/react_portfolio_ghpages/static/js/main.4eb76dc4.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react_portfolio_ghpages/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "2078eaf27a9b1a8bae3a",
+    "url": "/react_portfolio_ghpages/static/js/runtime~main.2078eaf2.js"
+  },
+  {
+    "revision": "96c992690fd1fe832fc06ecfdf452e35",
+    "url": "/react_portfolio_ghpages/index.html"
+  }
+];
